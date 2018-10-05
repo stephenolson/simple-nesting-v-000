065927074c4cd456e24297b1b26fa6dea1b3b451
Stephen Olson <stephenolson@gmail.com>
2018-10-05 21:55:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -139,7 +139,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:alan_kay][:languages] >> "Assembly"
+     programmer_hash[:alan_kay][:languages] << "Assembly"
      
      programmer_hash
 
